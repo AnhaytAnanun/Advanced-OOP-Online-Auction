b8a74cd845d81a3e60f0e5776eeffe5348c66c6d
AnhaytAnanun <anhaytananunn@gmail.com>
2015-09-19 12:21:51 +0400

    dispatcher singletone

--- Auction/src/com/mnr/auction/Dispatcher.java
+++ Auction/src/com/mnr/auction/Dispatcher.java
@@ -1,7 +1,41 @@
 package com.mnr.auction;
 
+import com.mnr.auction.archive.Archive;
+import com.mnr.auction.auction.AuctionList;
+import com.mnr.auction.store.Store;
+import com.mnr.auction.user.UserCatalog;
+
 /**
  * Created by anhaytananun on 19.09.15.
  */
 public class Dispatcher {
+    private static Dispatcher sharedDispatcher;
+
+    private Archive archive;
+    private AuctionList auctionList;
+    private UserCatalog userCatalog;
+    private Store store;
+
+    /**
+     * Constructor Methods
+     */
+
+    private Dispatcher() {
+        archive = new Archive();
+        auctionList = new AuctionList();
+        userCatalog = new UserCatalog();
+        store = new Store();
+    }
+
+    /**
+     * Singletone Methods
+     */
+
+    public static Dispatcher sharedDispatcher() {
+        if (sharedDispatcher == null) {
+            sharedDispatcher = new Dispatcher();
+        }
+
+        return sharedDispatcher;
+    }
 }
---
+++ Auction/src/com/mnr/auction/test/Test.java
@@ -0,0 +1,16 @@
+package com.mnr.auction.test;
+
+import com.mnr.auction.Dispatcher;
+
+/**
+ * Created by anhaytananun on 19.09.15.
+ */
+public class Test {
+    public static void main(String[] args) {
+        /**
+         * Create System
+         */
+
+        Dispatcher dispatcher = Dispatcher.sharedDispatcher();
+    }
+}
