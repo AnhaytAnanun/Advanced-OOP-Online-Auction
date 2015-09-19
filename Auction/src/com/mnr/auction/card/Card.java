57d80a224117dff9e34ef80e8565c939cadee0fa
MelkonChilingaryan <chilingaryan.melkon@gmail.com>
2015-09-19 12:16:54 +0400

    class decriptions

--- Auction/src/com/mnr/auction/card/Card.java
+++ Auction/src/com/mnr/auction/card/Card.java
@@ -4,4 +4,13 @@ package com.mnr.auction.card;
  * Created by anhaytananun on 19.09.15.
  */
 public class Card {
+
+    private int id;
+    private String date;
+    private int number;
+    private String type;
+
+    public void makePayement (){
+
+    }
 }
--- Auction/src/com/mnr/auction/item/item.java
+++ Auction/src/com/mnr/auction/item/item.java
@@ -4,4 +4,9 @@ package com.mnr.auction.item;
  * Created by Melkon on 19/09/2015.
  */
 public class item {
+    private int id;
+    private String name;
+    private String description;
+    private String startPrice;
+
 }
