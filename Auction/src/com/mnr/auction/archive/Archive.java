16755219c8d6c0a6eba768a7a1a6df38aef418b7
AnhaytAnanun <anhaytananunn@gmail.com>
2015-09-19 12:02:40 +0400

    archive, user,store

---
+++ Auction/src/com/mnr/auction/archive/Archive.java
@@ -0,0 +1,7 @@
+package com.mnr.auction.archive;
+
+/**
+ * Created by anhaytananun on 19.09.15.
+ */
+public class Archive {
+}
---
+++ Auction/src/com/mnr/auction/user/UserCatalog.java
@@ -0,0 +1,7 @@
+package com.mnr.auction.user;
+
+/**
+ * Created by anhaytananun on 19.09.15.
+ */
+public class UserCatalog {
+}
